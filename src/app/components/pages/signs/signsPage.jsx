32781d1694fd6a0590ce3682a36ee2391a81d890
medkov77 <medkov77@mail.ru>
2022-07-08 16:51:30 +0300

    price-price

--- src/app/components/pages/signs/signsPage.jsx
+++ src/app/components/pages/signs/signsPage.jsx
@@ -1,125 +1,134 @@
 import React, { useEffect } from "react";
 import {
-  Box,
-  Paper,
-  Typography,
-  Radio,
-  FormControl,
-  FormLabel,
-  RadioGroup,
-  FormControlLabel,
+    Box,
+    Paper,
+    Typography,
+    Radio,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    FormControlLabel,
 } from "@mui/material";
 import { useState } from "react";
-import { getPrice, getCurrentPrice } from "../../../../store/priceList";
+import { getSignsPrice } from "../../../../store/priceList";
 import { useDispatch, useSelector } from "react-redux";
 const SignsPage = ({
-  id,
-  gost,
-  name,
-  form,
-  sizes,
-  films,
-  description,
-  imgSrc,
+    id,
+    gost,
+    name,
+    form,
+    sizes,
+    films,
+    description,
+    imgSrc,
 }) => {
-  const dispatch = useDispatch();
-  const [data, setData] = useState({ size: 2, filmeType: "aCom" });
-  const [price, setPrice] = useState(
-    useSelector(getPrice("signs", form, data.size, data.filmeType))
-  );
+    const signsPiceList = useSelector(getSignsPrice());
 
-  const handleChange = ({ target }) => {
-    setData({ ...data, [target.name]: target.value });
-    dispatch(
-      getCurrentPrice({
-        name: "signs",
-        form: form,
-        size: data.size,
-        film: data.filmeType,
-      })
+    const dispatch = useDispatch();
+    const [data, setData] = useState({ size: 2, filmeType: "aCom" });
+    const [price, setPrice] = useState(
+        signsPiceList.form[form].size[data.size][data.filmeType]
     );
-  };
 
-  return (
-    <Paper elevation={3} className="card" sx={{ position: "relative" }}>
-      <Box p={2} sx={{ textAlign: "center" }}>
-        <Typography variant="h6" component={"h4"}>
-          Знак {gost} ГОСТ 52290-2004
-        </Typography>
-        <Typography variant="h6" component={"h4"}>
-          {name}
-        </Typography>
-        <img src={imgSrc} alt="1-1" className="card-img" />
-        <Box
-          p={1}
-          sx={{
-            position: "absolute",
-            bottom: "1rem",
-            left: "0",
-            right: "0",
-          }}
-        >
-          <FormControl>
-            <FormLabel id="size-radio-buttons-group">Типоразмер</FormLabel>
-            <RadioGroup
-              aria-labelledby="size-radio-buttons-group"
-              name="size"
-              value={data.size}
-              onChange={handleChange}
-            >
-              <div>
-                {Array.isArray(sizes)
-                  ? sizes.map((size) => (
-                      <FormControlLabel
-                        key={size}
-                        value={size}
-                        control={<Radio />}
-                        label={size}
-                      />
-                    ))
-                  : sizes}
-              </div>
-            </RadioGroup>
-            <FormLabel id="filmeType-radio-buttons-group">Тип пленки</FormLabel>
-            <RadioGroup
-              aria-labelledby="filmeType-radio-buttons-group"
-              name="filmeType"
-              value={data.filmeType}
-              onChange={handleChange}
-            >
-              <div>
-                <FormControlLabel
-                  value="aKom"
-                  control={<Radio />}
-                  label="А ком"
-                />
-                <FormControlLabel
-                  value="aIng"
-                  control={<Radio />}
-                  label="А инж"
-                />
-                <FormControlLabel value="B" control={<Radio />} label="Б" />
-                <FormControlLabel value="C" control={<Radio />} label="В" />
-              </div>
-            </RadioGroup>
-          </FormControl>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              mt: 2,
-            }}
-          >
-            <button className="card-btn">В корзину</button>
-            <Typography variant="h6" component={"h4"}>
-              цена {price}
-            </Typography>
-            <button className="card-btn">Подробнее</button>
-          </Box>
-        </Box>
-      </Box>
-    </Paper>
-  );
+    const handleChange = ({ target }) => {
+        setData(prevState => ({ ...prevState, [target.name]: target.value }));
+
+        console.log(data);
+    };
+    useEffect(() => {
+        setPrice(signsPiceList.form[form].size[data.size][data.filmeType]);
+    }, [data]);
+    return (
+        <Paper elevation={3} className="card" sx={{ position: "relative" }}>
+            <Box p={2} sx={{ textAlign: "center" }}>
+                <Typography variant="h6" component={"h4"}>
+                    Знак {gost} ГОСТ 52290-2004
+                </Typography>
+                <Typography variant="h6" component={"h4"}>
+                    {name}
+                </Typography>
+                <img src={imgSrc} alt="1-1" className="card-img" />
+                <Box
+                    p={1}
+                    sx={{
+                        position: "absolute",
+                        bottom: "1rem",
+                        left: "0",
+                        right: "0",
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel id="size-radio-buttons-group">
+                            Типоразмер
+                        </FormLabel>
+                        <RadioGroup
+                            aria-labelledby="size-radio-buttons-group"
+                            name="size"
+                            value={data.size}
+                            onChange={handleChange}
+                        >
+                            <div>
+                                {Array.isArray(sizes)
+                                    ? sizes.map(size => (
+                                          <FormControlLabel
+                                              key={size}
+                                              value={size}
+                                              control={<Radio />}
+                                              label={size}
+                                          />
+                                      ))
+                                    : sizes}
+                            </div>
+                        </RadioGroup>
+                        <FormLabel id="filmeType-radio-buttons-group">
+                            Тип пленки
+                        </FormLabel>
+                        <RadioGroup
+                            aria-labelledby="filmeType-radio-buttons-group"
+                            name="filmeType"
+                            value={data.filmeType}
+                            onChange={handleChange}
+                        >
+                            <div>
+                                <FormControlLabel
+                                    value="aCom"
+                                    control={<Radio />}
+                                    label="А ком"
+                                />
+                                <FormControlLabel
+                                    value="aIng"
+                                    control={<Radio />}
+                                    label="А инж"
+                                />
+                                <FormControlLabel
+                                    value="B"
+                                    control={<Radio />}
+                                    label="Б"
+                                />
+                                <FormControlLabel
+                                    value="C"
+                                    control={<Radio />}
+                                    label="В"
+                                />
+                            </div>
+                        </RadioGroup>
+                    </FormControl>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            mt: 2,
+                        }}
+                    >
+                        <button className="card-btn">В корзину</button>
+                        <Typography variant="h6" component={"h4"}>
+                            {`цена ${price} р`}
+                        </Typography>
+                        <button className="card-btn">Подробнее</button>
+                    </Box>
+                </Box>
+            </Box>
+        </Paper>
+    );
 };
-
 export default SignsPage;
--- src/store/priceList.js
+++ src/store/priceList.js
@@ -1,64 +1,63 @@
 import { createSlice } from "@reduxjs/toolkit";
 import getPricesList from "../app/api/price";
 const priceListSlice = createSlice({
-  name: "priceList",
-  initialState: {
-    entities: null,
-    isLoading: true,
-    error: null,
-    lastFetch: null,
-  },
-  reducers: {
-    priceListRequested: (state) => {
-      state.isLoading = true;
+    name: "priceList",
+    initialState: {
+        entities: null,
+        isLoading: true,
+        error: null,
+        lastFetch: null,
     },
+    reducers: {
+        priceListRequested: state => {
+            state.isLoading = true;
+        },
 
-    priceListRequested: (state) => {
-      state.isLoading = true;
+        priceListRequested: state => {
+            state.isLoading = true;
+        },
+        priceListReceived: (state, action) => {
+            state.entities = action.payload;
+            state.isLoading = false;
+        },
+        priceListRequestFailed: (state, action) => {
+            state.error = action.payload;
+            state.isLoading = false;
+        },
+        getCurrentPrice: (state, action) => {
+            console.log(action.payload, "pay");
+            console.log(state);
+            const { name, form, size, film } = action.payload;
+            return state.entities[name].form[form].size[size][film];
+        },
     },
-    priceListReceived: (state, action) => {
-      state.entities = action.payload;
-      state.isLoading = false;
-    },
-    priceListRequestFailed: (state, action) => {
-      state.error = action.payload;
-      state.isLoading = false;
-    },
-    getCurrentPrice: (state, action) => {
-      console.log(action.payload, "pay");
-      console.log(state);
-      const { name, form, size, film } = action.payload;
-      return state.entities[name].form[form].size[size][film];
-    },
-  },
 });
 
 const { reducer: priceListReducer, actions } = priceListSlice;
 const {
-  priceListRequested,
-  priceListReceived,
-  priceListRequestFailed,
-  getCurrentPrice,
+    priceListRequested,
+    priceListReceived,
+    priceListRequestFailed,
+    getCurrentPrice,
 } = actions;
 
 export const loadPriceListList = () => async (dispatch, getState) => {
-  dispatch(priceListRequested());
+    dispatch(priceListRequested());
 
-  try {
-    const content = getPricesList();
-    dispatch(priceListReceived(content));
-  } catch (error) {
-    dispatch(priceListRequestFailed(error.message));
-  }
+    try {
+        const content = getPricesList();
+        dispatch(priceListReceived(content));
+    } catch (error) {
+        dispatch(priceListRequestFailed(error.message));
+    }
 };
 
-export const getPrice = (name, form, size, film) => (state) => {
-  const currentPrice =
-    state.priceList.entities[name].form[form].size[size][film];
-  return currentPrice;
+export const getSignsPrice = (name, form, size, film) => state => {
+    // state.priceList.entities[name].form[form].size[size][film];
+    return state.priceList.entities.signs;
 };
-export const getpriceListLoadingStatus = () => (state) =>
-  state.priceList.isLoading;
+export const getpriceListLoadingStatus = () => state =>
+    state.priceList.isLoading;
 
 export default priceListReducer;
 export { getCurrentPrice };
